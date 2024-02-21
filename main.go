8866abd719cd0d9af956e876e19eece8ba33348d
Sergey <pav220521@gmail.com>
2024-02-21 20:52:45 +0300

    dz_4

--- main.go
+++ main.go
@@ -1,117 +1,71 @@
-// Напишите программу, которая будет хранить ваши url. На основании созданного шаблона допишите код, который позволяет
-//добавлять новые ссылки, удалять и выводить список.
-//Для решения задачи используйте структуры. Обязательные поля структуры должны быть дата добавления, имя ссылки,
-//теги для ссылки через запятую и сам url.
+// Вам предстоит написать простую версию кеша. Не пугайтесь это самая простая реализация какую можно придумать. Самое
+// главное это запомнить зачем нам нужны интерфейсы. Если вы загляните в код, то увидите несколько структур, вам нужно
+// доделать реализацию cacheImpl. Она должна хранить все значения, которые вы захотите разместить в этой структуре по
+// ключ и значению. Используйте структуры данных, которые знаете.
+// В функции main мы создаем некий аналог базы данных куда передаем наш кеш. Вам нужно создать структуру кеша и
+// наполнить ее данными, которые добавляются в структуру dbsImpl. Далее проверить, что данные возвращаются с вашего кеша
+// *********** Задача со звездочкой ***********
+// Задача не сложная, постарайтесь выполнить!
+// Реализуйте удаление старых ключей по времени. Для этого вам возможно понадобятся функции работы с временем,а также
+// возможно вы захотите создать структуру в которой будут хранится значения и временный метки.
+// Удалять ключи можно например при операциях Get. Проверять временную метку и удалять, если ключ старый.
+
 
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strings"
 	"time"
-
-	"github.com/eiannone/keyboard"
 )
 
-type Item struct {
-	Link string
-	URL  string
-	Tags string
-	Date string
+type Cache interface {
+	Get(k string) (string, bool)
+	Set(k,v string)}
+
+type cacheDate struct {
+	cacheVal string
+	date time.Time
 }
 
-func createItem(url, link, tags string) Item {
-	date := time.Now().Format(time.DateTime)
-	return Item{URL: url, Link: link, Tags: tags, Date: date}
-}
+var _ Cache = (*cacheImpl)(nil)
 
-func (i Item) ShowItem() {
-	fmt.Printf("Имя: <%s>\nURL: <%s>\nТеги: <%s>\nДата: <%s>\n", i.Link, i.URL, i.Tags, i.Date)
-}
-
-type URLmap map[string]Item
-
-func (u URLmap) AddURL(i Item) {
-	u[i.Link] = i
-}
-
-func (u URLmap) DelURL(name string) {
-	delete(u, name)
-}
-
-func (u URLmap) ShowAllURLs() {
-	fmt.Printf("\nВсего закладок: %d\n", len(u))
-	for _, v := range u {
-		v.ShowItem()
+func newCacheImpl() *cacheImpl {
+	return &cacheImpl{
+		cacheMap: make(map[string]cacheDate),
 	}
+}
+
+type cacheImpl struct {
+	cacheMap map[string]cacheDate
+}
+
+func (c *cacheImpl) Get(key string) (string, bool) {
+	for k,v := range c.cacheMap {
+		if k == key {
+			if time.Since(v.date) > 5 * time.Second {
+				delete(c.cacheMap, k)
+			}
+		}
+	}
+	v, ok := c.cacheMap[key]
+	if ok {
+		return fmt.Sprintf("value: %s for key: %s.", v.cacheVal, key), ok
+	}
+	return fmt.Sprintf("no value for key: %s.", key), ok
+}
+
+func (c *cacheImpl) Set(key, value string) {
+	c.cacheMap[key] = cacheDate{cacheVal: value, date: time.Now() }
 }
 
 func main() {
-	defer func() {
-		// Завершаем работу с клавиатурой при выходе из функции
-		_ = keyboard.Close()
-	}()
-
-	fmt.Println("Программа для добавления url в список")
-	fmt.Println("Для выхода и приложения нажмите Esc")
-
-	myMap := URLmap{}
-
-OuterLoop:
-	for {
-		// Подключаем отслеживание нажатия клавиш
-		if err := keyboard.Open(); err != nil {
-			log.Fatal(err)
-		}
-
-		char, key, err := keyboard.GetKey()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		switch char {
-		case 'a':
-			if err := keyboard.Close(); err != nil {
-				log.Fatal(err)
-			}
-
-			// Добавление нового url в список хранения
-			fmt.Println("Введите новую запись в формате <url описание теги>")
-
-			reader := bufio.NewReader(os.Stdin)
-			text, _ := reader.ReadString('\n')
-			args := strings.Fields(text)
-			if len(args) < 3 {
-				fmt.Println("Введите правильный аргументы в формате url описание теги")
-				continue OuterLoop
-			}
-
-			newURL := createItem(args[0], args[1], args[2])
-			myMap.AddURL(newURL)
-		case 'r':
-			if err := keyboard.Close(); err != nil {
-				log.Fatal(err)
-			}
-			// Удаление url из списка хранения
-			fmt.Println("Введите имя ссылки, которое нужно удалить")
-
-			reader := bufio.NewReader(os.Stdin)
-			text, _ := reader.ReadString('\n')
-			_ = text
-
-			myMap.DelURL(text)
-		case 'l':
-			// Вывод списка добавленных url. Выведите количество добавленных url и список с данными url
-			myMap.ShowAllURLs()
-
-		default:
-			// Если нажата Esc выходим из приложения
-			if key == keyboard.KeyEsc {
-				return
-			}
-		}
-	}
+	myCache := newCacheImpl()
+	myCache.Set("hello", "world")
+	myCache.Set("test", "my test")
+	fmt.Println(myCache.Get("hello"))
+	fmt.Println(myCache.Get("test"))
+	time.Sleep(6 * time.Second)
+	fmt.Println("After 6 second")
+	fmt.Println(myCache.Get("hello"))
+	fmt.Println(myCache.Get("test"))
 }
